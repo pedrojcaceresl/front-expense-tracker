4b7b81f70bceb3821b08f68635f9a51d3ad85240
pedro <pedro.jcaceresl@gmail.com>
2025-02-02 18:02:44 -0300

    add PaginaEjemplo component with example content

---
+++ src/app/pages/PaginaEjemplo.jsx
@@ -0,0 +1,12 @@
+
+export const PaginaEjemplo = () => {
+  return (
+		<div>
+			<h1>Esta es mi súper página de ejemplo</h1>
+			<img
+				src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnitogg46slUOT5lHy-2cq0P1BaShFHvdg-g&s"
+				alt="Imagen de gato superheroe"
+			/>
+		</div>
+  );
+}
