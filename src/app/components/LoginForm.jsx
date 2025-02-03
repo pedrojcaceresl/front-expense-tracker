23a03aabb1a3f9f5bbbeaf74bd4686c38aea4be3
RomyCardozo <romycardozopy@gmail.com>
2025-02-02 23:29:38 -0300

    mejorar diseño del LoginForm: agregar logo, actualizar etiquetas y clases de estilo

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -4,14 +4,18 @@ export const LoginForm = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <form>
         <div className="bg-blue-200 rounded-xl p-10 shadow-lg shadow-blue-500/50">
+          <div className=" flex flex-col items-center justify-center p-2">
+            <img src="https://static.vecteezy.com/system/resources/previews/017/566/984/non_2x/operating-expenses-glyph-two-color-icon-vector.jpg" alt="logo" className=" w-30 rounded-full" />
+          </div>
           <h1 className="text-2xl flex flex-col items-center justify-center p-4">Bienvenido</h1>
+
           <div className="p-2">
-            <label>Email</label>
-            <input type="email" name="email" id="email" className=" rounded-xl border-2 flex align-items w-full" />
+            <label>Correo</label>
+            <input type="email" name="correo" id="correo" className=" rounded-sm border-2 border-gray-500 align-items w-full" />
           </div>
           <div className=" p-2">
-            <label>Password </label>
-            <input type="password" name="password" id="password" className=" rounded-xl border-2 flex align-items w-full" />
+            <label>Contraseña </label>
+            <input type="password" name="contraseña" id="contraseña" className=" rounded-sm border-2 border-gray-500 align-items w-full" />
           </div>
 
           <div className="flex align-items justify-center p-2">
@@ -19,11 +23,11 @@ export const LoginForm = () => {
             >Iniciar sesion</button>
           </div>
 
-          <div>
-            <p className="text-blue-500 ">Aun no tienes una cuenta?</p> 
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-blue-500 ">Aun no tienes una cuenta?</p>
             <a href="#" className="text-blue-500 underline underline-offset-2 flex flex-col items-center">Registrate</a>
           </div>
-           
+
 
         </div>
       </form>
