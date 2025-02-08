9b907869b93e348bf6d3d6c6c5bf324995fd876c
RomyCardozo <romycardozopy@gmail.com>
2025-02-08 19:05:33 -0300

    mejorar diseño de formularios: actualizar estilos y agregar enlaces de navegación en LoginForm y RegisterForm

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export const LoginForm = () => {
   const imageUrl = "https://static.vecteezy.com/system/resources/previews/017/566/984/non_2x/operating-expenses-glyph-two-color-icon-vector.jpg";
   return (
-    <div className=' flex flex-col items-center justify-center h-screen '>
+    <div className=' flex flex-col items-center justify-center h-screen bg-gradient-to-br to-blue-50 from-indigo-50'>
       <form>
-        <div className=" bg-blue-200 flex flex-col items-center justify-center shadow-lg shadow-gray-800 p-5 rounded-lg w-80 ">
+        <div className=" bg-white flex flex-col items-center justify-center shadow-lg shadow-gray-800 p-5 rounded-lg w-80 ">
           <img className=' w-40 p-3 rounded-full' src={imageUrl} alt="Logo" />
-          <h1 className='text-2xl'>Bienvenido</h1>
+          <h1 className='text-2xl font-medium text-indigo-950'>Bienvenido</h1>
           <div className='p-2 flex flex-col items-start w-full'>
             <label className='mb-1'> Email</label>
             <input type='email' name='email' id='email' className='rounded-xl px-3 py-1 border border-gray-800 w-full'/>
@@ -18,13 +19,13 @@ export const LoginForm = () => {
           </div>
 
           <div>
-            <button className='bg-blue-300 rounded-2xl p-2 hover:bg-blue-400 hover:cursor-pointer transition duration-300'>
+            <button className='bg-blue-950 rounded-lg text-white p-2 hover:bg-blue-900 hover:cursor-pointer transition duration-300'>
               Iniciar sesion
             </button>
           </div>
           <div className='p-4 flex flex-col items-center text-sm'>
             <p>Aun no tienes una cuenta?</p>
-            <a href='#' className='font-bold hover:text-blue-900'>Registrate</a>
+            <Link to="/auth/register" className=' text-blue-500 font-medium hover:text-blue-900'>Registrate</Link>
           </div>
         </div>
       </form>
