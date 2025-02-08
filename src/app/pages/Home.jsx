11d70ddabb83e4058b381be3cc7ae5ec240aa55e
pedro <pedro.jcaceresl@gmail.com>
2025-02-08 20:33:07 -0300

    refactor Home component: improve code readability and formatting

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -32,78 +32,78 @@ export const Home = () => {
     ]
 
     return (
-        <div className=' min-h-screen mt-2'>
+		<div className=" min-h-screen mt-2">
+			{/* navbar */}
+			<nav className="bg-white shadow-sm flex justify-between items-center  w-full">
+				<div className="flex flex-row items-center">
+					<img
+						src={imageUrl}
+						alt="logoHome"
+						className="w-20 ml-4 mb-4"
+					/>
+					<h1 className=" text-2xl font-serif text-blue-950  font-bold ml-4 mb-4">
+						Gestor de gastos
+					</h1>
+				</div>
 
-            {/* navbar */}
-            <nav className='bg-white shadow-sm flex justify-between items-center  w-full' >
-                <div className='flex flex-row items-center'>
-                    <img src={imageUrl} alt='logoHome' className='w-20 ml-4 mb-4' />
-                    <h1 className=' text-2xl font-serif text-blue-950  font-bold ml-4 mb-4'>Gestor de gastos</h1>
-                </div>
+				<div className="flex gap-10 p-4">
+					<Link
+						to="/auth/login"
+						className="bg-white text-gray-500 rounded-sm px-4 py-2 hover:bg-gray-200 transition duration-500"
+					>
+						Iniciar sesión
+					</Link>
+					<Link
+						to="/auth/register"
+						className="bg-blue-950 rounded-sm text-white px-4 py-2 hover:bg-blue-900 transition duration-500"
+					>
+						Registrate
+					</Link>
+				</div>
+			</nav>
 
-                <div className='flex gap-10 p-4'>
-                    <Link to="/auth/login" className='bg-white text-gray-500 rounded-sm px-4 py-2 hover:bg-gray-200 transition duration-500'>
-                        Iniciar sesión
-                    </Link>
-                    <Link to="/auth/register" className='bg-blue-950 rounded-sm text-white px-4 py-2 hover:bg-blue-900 transition duration-500' >
-                        Registrate
-                    </Link>
-                </div>
-            </nav>
+			{/* presentacion */}
+			<div className="flex flex-col mt-10 mb-10 justify-top ">
+				<div className=" text-3xl font-extrabold text-gray-800 text-center">
+					<h2>
+						<span className="block">Controla tus finanzas</span>
+						<span className="text-blue-800">
+							de manera eficiente
+						</span>
+					</h2>
+					<p className="text-gray-500 font-normal text-xl text-center">
+						Gestiona tus gastos, establece metas y toma el control
+						de tu <br />
+						dinero con la aplicación más completa del mercado.
+					</p>
+				</div>
+			</div>
 
+			{/* Caracteristicas de la app */}
 
-            {/* presentacion */}
-            <div className='flex flex-col mt-10 mb-10 justify-top '>
-                <div className=' text-3xl font-extrabold text-gray-800 text-center'>
-                    <h2>
-                        <span className='block'>
-                            Controla tus finanzas
-                        </span>
-                        <span className='text-blue-800'>
-                            de manera eficiente
-                        </span>
-                    </h2>
-                    <p className='text-gray-500 font-normal text-xl text-center'>
-                        Gestiona tus gastos, establece metas y toma el control de tu <br />
-                        dinero con la aplicación más completa del mercado.
-                    </p>
-
-                </div>
-            </div>
-
-
-            {/* Caracteristicas de la app */}
-
-            <div className=''>
-                {
-                    features.map((index) => (
-                        <div className='flex justify-center items-center gap-12 px-8 mt-12 flex-wrap'>
-                            <div key={index.title} className='flex flex-col items-center text-center bg-white p-6 rounded-lg shadow-lg w-200'>
-                                <div className='text-blue-950'>
-                                    {index.icon}
-                                </div>
-                                <h2 className='font-bold'>
-                                    {index.title}
-                                </h2>
-                                <p className=''>
-                                    {index.description}
-                                </p>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className=' mt-10 mb-5 flex flex-col items-center justify-end'>
-                <footer className='bg-white text-center w-60 h-10'>
-
-                    <p className='text-gray-500'>
-                        © 2025 Todos los derechos reservados.
-                    </p>
-
-                </footer>
-            </div>
-        </div>
-    )
+			<div className="">
+				{features.map((index) => (
+					<div
+						key={index.title}
+						className="flex justify-center items-center gap-12 px-8 mt-12 flex-wrap"
+					>
+						<div className="flex flex-col items-center text-center bg-white p-6 rounded-lg shadow-lg w-200">
+							<div className="text-blue-950">{index.icon}</div>
+							<h2 className="font-bold">{index.title}</h2>
+							<p className="">{index.description}</p>
+						</div>
+					</div>
+				))}
+			</div>
+			<div className=" mt-10 mb-5 flex flex-col items-center justify-end">
+				<footer className="bg-white text-center w-60 h-10">
+					<p className="text-gray-500">
+						© 2025 Todos los derechos reservados.
+					</p>
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 {/* <div className='flex flex-col items-center justify-center mt-10'>
