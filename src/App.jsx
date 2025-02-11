e6016fc3ec00229d0b8915ca3e48de88a572e834
Pedro Cáceres <76101142+pedrojcaceresl@users.noreply.github.com>
2025-02-10 23:44:11 -0300

    Revert "Feat/mejorar diseño home"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { Route, Routes } from "react-router"
-import { AuthPage } from "./app/pages/AuthPage"
-import { Home } from "./app/pages/Home"
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth/*" element={<AuthPage />} />
-      </Routes>
-
+      
     </>
   )
 }
